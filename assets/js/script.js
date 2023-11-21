97f5a8c70aaf16981293591ab4c30164d7e7b028
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2023-11-21 10:51:28 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
 
-    if (selectedValue === "TatCa") {
+    if (selectedValue === "tatca") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
       filterItems[i].classList.add("active");
