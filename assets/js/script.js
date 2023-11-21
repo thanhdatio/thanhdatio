4701ccf1a05a3d3b9c9650bfaa2db6eaa213a0cc
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2023-11-21 11:26:50 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,15 +143,15 @@ for (let i = 0; i < formInputs.length; i++) {
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
-//const pages = document.querySelectorAll("[data-page]");
+const pages = document.querySelectorAll("[data-page]");
 //const pages="";
-const pages=["giới thiệu","lịch trình","mục tiêu","blog","liên hệ"];
+const checkpages=["giới thiệu","lịch trình","mục tiêu","blog","liên hệ"];
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      if (this.innerHTML.toLowerCase() === checkpages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
