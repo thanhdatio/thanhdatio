fffbb98febcfc0273b7310b46184b93d94e3651b
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2023-11-22 11:44:17 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,13 +142,14 @@ for (let i = 0; i < formInputs.length; i++) {
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
 const pages_about = document.getElementById("about");
 const pages_resume = document.getElementById("resume");
 const pages_portfolio = document.getElementById("portfolio");
 const pages_blog = document.getElementById("blog");
 const pages_contact = document.getElementById("contact"); 
 alert("ok -1");
-let pages=[pages_about,pages_resume,pages_portfolio,pages_blog,pages_contact];
+//let pages=[pages_about,pages_resume,pages_portfolio,pages_blog,pages_contact];
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
